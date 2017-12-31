9a3ece61f175674f3c0c223ae2a9e3738a8e2102
Pablo Tirado <pablotr87@gmail.com>
2017-12-31 17:48:39 +0100

    Add Lombok annotations.

--- src/main/java/com/ptirador/concessionaire/model/MenuWrapper.java
+++ src/main/java/com/ptirador/concessionaire/model/MenuWrapper.java
@@ -1,6 +1,9 @@
 package com.ptirador.concessionaire.model;
 
 import com.ptirador.concessionaire.util.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,6 +13,9 @@ import java.util.List;
  *
  * @author ptirador
  */
+@AllArgsConstructor
+@Getter
+@Setter
 public class MenuWrapper implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -25,31 +31,4 @@ public class MenuWrapper implements Serializable {
      * Menus list.
      */
     private List<Menu> menusList;
-
-    /**
-     * Constructor.
-     *
-     * @param typeId
-     * @param menusList
-     */
-    public MenuWrapper(Integer typeId, List<Menu> menusList) {
-        this.typeId = typeId;
-        this.menusList = menusList;
-    }
-
-    public Integer getTypeId() {
-        return typeId;
-    }
-
-    public void setTypeId(Integer typeId) {
-        this.typeId = typeId;
-    }
-
-    public List<Menu> getMenusList() {
-        return menusList;
-    }
-
-    public void setMenusList(List<Menu> menusList) {
-        this.menusList = menusList;
-    }
 }
